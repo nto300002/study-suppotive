335a905b03b3f2e6b109e4d26718b34472ad8805
nto300002 <samonkntd@gmail.co>
2021-12-18 23:25:40 +0900

    home link add

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,11 +1,18 @@
 import { Head, BlitzLayout } from "blitz"
+import { Link, Routes } from "blitz"
 
-const Layout: BlitzLayout<{title?: string}> = ({ title, children }) => {
+const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   return (
     <>
       <Head>
         <title>{title || "supportive-push"}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <ul>
+          <li>
+            <Link href="/">
+              <a>home</a>
+            </Link>
+          </li>
+        </ul>
       </Head>
 
       {children}
